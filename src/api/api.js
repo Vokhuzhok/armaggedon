b49ddb39809ed1cd97048a38f54c988a91c037b2
Denis Kozhukhov <kurkoja@bk.ru>
2021-04-26 08:07:40 +0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ const date = calculatingTheDate();
 export const asteroidsAPI = {
   getFirstAsteroids() {
     return fetch(
-      `https://api.nasa.gov/neo/rest/v1/feed?start_date=${date.year}-${date.month}-${date.day}&end_date=${date.year}-${date.month}-${date.day}&api_key=SeCrF9HdnFGIOZnfeQo9uPTyU2bNcVl3rkwZM9Ld`
+      `https://api.nasa.gov/neo/rest/v1/feed?start_date=${date.year}-${date.month}-${date.day}&end_date=${date.year}-${date.month}-${date.day}&api_key=DEMO_KEY`
     )
       .then((response) => response.json())
       .then((data) => {
